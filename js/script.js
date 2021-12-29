fe839a2b6bf35063dcc7453515714108842df0a0
Vivek-4869 <96434736+Vivek-4869@users.noreply.github.com>
2021-12-29 22:44:19 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+console.log('Created by Ai Coder')
+
+/*Template Inheritance */
+
+$(function(){
+    $("#nav-placeholder").load("navbar.html");
+    $("#footer-placeholder").load("footer.html");
+    });
+    
