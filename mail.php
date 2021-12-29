16532a736c274fb9c769b0f292e04d1865125559
Vivek-4869 <96434736+Vivek-4869@users.noreply.github.com>
2021-12-29 22:56:29 +0530

    Update mail.php

--- mail.php
+++ mail.php
@@ -8,10 +8,9 @@ $to = "viveksharma40649@gmail.com";
 $subject = "Mail From website";
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
 $headers = "From: noreply@yoursite.com" . "\r\n" .
-"CC: somebodyelse@example.com";
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
 }
 //redirect
-header("Location:contact-message.html");
+header("Location:message.html");
 ?>
